4331d4794c46cdcbdebd3109e81d69cbce27cacd
Erwan <erwan.lebras27@gmail.com>
2025-04-20 00:50:22 +0200

    refactor : improve the river mesh + fragment

--- assets/shaders/river_f.glsl
+++ assets/shaders/river_f.glsl
@@ -12,4 +12,5 @@ out vec4 fragColor;
 void main() {
     fragColor = texture(u_texture, vec2(v_texCoords.x, mod(v_texCoords.y - u_time, 1.f)));
     fragColor *= texture(u_textureColorMapWater, vec2(v_worldCoords.x, v_worldCoords.y));
+    fragColor.rgb = 1.0 - 2.0 * (1.0 - vec3(0.6)) * (1.0 - fragColor.rgb);
 }
