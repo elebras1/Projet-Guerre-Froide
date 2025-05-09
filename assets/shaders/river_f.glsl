e97464ac96a42d90854ee2ac9a2268f6475b9207
elebras1 <erwan.lebras27@gmail.com>
2025-05-09 18:33:56 +0200

    chore : remove the mix color

--- assets/shaders/river_f.glsl
+++ assets/shaders/river_f.glsl
@@ -11,8 +11,6 @@ uniform float u_zoom;
 
 out vec4 fragColor;
 
-const vec3 darkBlue = vec3(0.0, 0.0, 0.4);
-
 void main() {
     vec2 scrolledCoords = vec2(v_texCoords.x, v_texCoords.y - u_time);
     fragColor = texture(u_texture, scrolledCoords);
@@ -20,8 +18,6 @@ void main() {
     fragColor.rgb = vec3(1.0) - vec3(0.8) * (1.0 - fragColor.rgb);
 
     float zoomBlend = clamp((u_zoom - 0.8) * 0.37037, 0.0, 1.0);
-
-    fragColor.rgb = mix(fragColor.rgb, darkBlue, zoomBlend);
 
     float widthThreshold = mix(0.25, 0.9, zoomBlend);
     float visibilityMask = (u_zoom > 0.8) ? step(widthThreshold, v_width) : 1.0;
