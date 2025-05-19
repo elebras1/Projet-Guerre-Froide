e3c55804a4ecb4f871aa1d6c103c0d74b05bfa78
elebras1 <erwan.lebras27@gmail.com>
2025-05-19 18:52:20 +0200

    chore : move opacity text from fragment to vertex

--- assets/shaders/font_f.glsl
+++ assets/shaders/font_f.glsl
@@ -2,8 +2,8 @@
 
 in vec2 v_texCoords;
 in vec4 v_color;
+in float opacity;
 uniform sampler2D u_texture;
-uniform float u_zoom;
 
 const float smoothing = 0.02;
 
@@ -12,11 +12,9 @@ out vec4 fragColor;
 void main() {
     vec4 texColor = texture(u_texture, v_texCoords);
 
-    float visibility = smoothstep(0.10, 0.25, u_zoom);
-
     float distance = texColor.a;
     float alpha = smoothstep(0.50 - smoothing, 0.50 + smoothing, distance);
 
     vec4 text = vec4(texColor.rgb, v_color.a * alpha);
-    fragColor = text * visibility;
+    fragColor = text * opacity;
 }
--- assets/shaders/font_v.glsl
+++ assets/shaders/font_v.glsl
@@ -5,12 +5,20 @@ in vec4 a_color;
 in vec2 a_texCoord0;
 
 uniform mat4 u_projTrans;
+uniform float u_zoom;
 
 out vec4 v_color;
 out vec2 v_texCoords;
+out float opacity;
+
+const float thickness = 0.04;
 
 void main() {
     v_color = a_color;
     v_texCoords = a_texCoord0;
+    opacity = 1.f;
+    if(u_zoom < 0.5f) {
+        opacity = exp(-(u_zoom * 50.f - 1.f/thickness) * (u_zoom * 50.f - 1.f/thickness) * 0.01f);
+    }
     gl_Position = u_projTrans * a_position;
 }
