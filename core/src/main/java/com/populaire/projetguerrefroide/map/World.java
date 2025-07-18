d65b1077061316cdda88cf1d2a804976d4b9338d
elebras1 <erwan.lebras27@gmail.com>
2025-07-19 00:05:51 +0200

    chore : timor, malta an vietnam change controller, owner and alliance

--- core/src/main/java/com/populaire/projetguerrefroide/map/World.java
+++ core/src/main/java/com/populaire/projetguerrefroide/map/World.java
@@ -169,6 +169,7 @@ public class World implements Disposable {
 
     public boolean selectProvince(short x, short y) {
         this.selectedProvince = this.getProvince(x, y);
+        System.out.println("\n" + this.selectedProvince);
         return this.selectedProvince != null;
     }
 
