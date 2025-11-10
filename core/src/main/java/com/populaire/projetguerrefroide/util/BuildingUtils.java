88eea4b23fc75386364a152599488077ef611e48
elebras1 <erwan.lebras27@gmail.com>
2025-11-10 21:06:17 +0100

    rename transport_building to multimodal_transport

--- core/src/main/java/com/populaire/projetguerrefroide/util/BuildingUtils.java
+++ core/src/main/java/com/populaire/projetguerrefroide/util/BuildingUtils.java
@@ -9,7 +9,7 @@ public class BuildingUtils {
             case "inorganic_chemicals_factory", "plastics_factory", "pharmaceuticals_factory" -> "purple";
             case "integrated_circuits_factory", "electronic_components_factory", "computers_factory", "communication_factory" -> "blue";
             case "machine_parts_factory", "automobiles_factory" -> "darkblue";
-            case "transport_factory", "ships_factory", "aeroplanes_factory" -> "yellow";
+            case "multimodal_transport", "ships_factory", "aeroplanes_factory" -> "yellow";
             case "ammunition_factory", "weaponry_factory", "armoured_vehicles_factory", "military_satellites_factory", "centrifuges" -> "cyan";
             case "aluminium_factory" -> "orange";
             default -> null;
