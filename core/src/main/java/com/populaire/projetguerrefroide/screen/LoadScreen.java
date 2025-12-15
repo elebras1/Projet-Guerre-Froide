67581cfb49f8405f3b423c9bdce58f4ff543f0f1
elebras1 <erwan.lebras27@gmail.com>
2025-12-15 18:53:02 +0100

    improve loading screen

--- core/src/main/java/com/populaire/projetguerrefroide/screen/LoadScreen.java
+++ core/src/main/java/com/populaire/projetguerrefroide/screen/LoadScreen.java
@@ -7,23 +7,22 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.github.tommyettinger.ds.ObjectList;
 import com.monstrous.gdx.webgpu.scene2d.WgStage;
 import com.populaire.projetguerrefroide.service.ConfigurationService;
 import com.populaire.projetguerrefroide.service.GameContext;
 import com.populaire.projetguerrefroide.service.WorldService;
-import com.populaire.projetguerrefroide.util.Logging;
 
-import java.util.List;
 import java.util.Random;
+
+import static com.populaire.projetguerrefroide.screen.LoadingStep.*;
 
 public class LoadScreen implements Screen {
     private final Stage stage;
-    private final Skin skin;
     private final ScreenManager screenManager;
     private final GameContext gameContext;
     private final WorldService worldService;
     private final ConfigurationService configurationService;
+    private LoadingStep loadingStep;
 
     public LoadScreen(ScreenManager screenManager, GameContext gameContext, ConfigurationService configurationService) {
         this.screenManager = screenManager;
@@ -31,46 +30,52 @@ public class LoadScreen implements Screen {
         this.worldService = new WorldService(gameContext);
         this.configurationService = configurationService;
         this.gameContext.getCursorManager().animatedCursor("busy");
+
         AssetManager assetManager = this.gameContext.getAssetManager();
         assetManager.load("loadingscreens/loadingscreens_skin.json", Skin.class);
         this.stage = new WgStage();
         Gdx.input.setInputProcessor(this.stage);
-        List<String> loadingImageNames = ObjectList.with("load_1", "load_2", "load_3", "load_4", "load_5", "load_6", "load_7", "load_8", "load_9", "load_10", "load_11", "load_12");
-        Random random = new Random();
+
         Table rootTable = new Table();
         assetManager.finishLoading();
-        this.skin = assetManager.get("loadingscreens/loadingscreens_skin.json", Skin.class);
+        Skin skin = assetManager.get("loadingscreens/loadingscreens_skin.json", Skin.class);
         rootTable.setFillParent(true);
-        Drawable background = this.skin.getDrawable(loadingImageNames.get(random.nextInt(loadingImageNames.size())));
+        Random random = new Random();
+        Drawable background = skin.getDrawable("load_" + random.nextInt(1, 12));
         rootTable.setBackground(background);
         this.stage.addActor(rootTable);
-        Gdx.graphics.setForegroundFPS(1);
+        this.loadingStep = LOADING_ASSETS;
     }
 
     @Override
     public void show() {
         this.configurationService.loadGameAssets(gameContext.getAssetManager());
-        long startTime = System.currentTimeMillis();
-        this.worldService.createWorld();
-
-        Gdx.app.postRunnable(() -> {
-            this.gameContext.getSettings().applyGraphicsSettings();
-            this.screenManager.showNewGameScreen(this.worldService);
-        });
-
-        long endTime = System.currentTimeMillis();
-        Logging.getLogger("LoadScreen").info("World load: " + (endTime - startTime) + "ms");
-
-        this.gameContext.getAssetManager().finishLoading();
     }
 
     @Override
     public void render(float delta) {
-        this.gameContext.getEcsWorld().progress(delta);
-        this.stage.act();
-        this.stage.draw();
-
-        this.gameContext.getCursorManager().update(delta);
+        switch (this.loadingStep) {
+            case LOADING_ASSETS -> {
+                this.gameContext.getEcsWorld().progress(delta);
+                this.stage.act();
+                this.stage.draw();
+                this.gameContext.getCursorManager().update(delta);
+                if (this.gameContext.getAssetManager().update()) {
+                    this.loadingStep = CREATING_WORLD;
+                }
+            }
+            case CREATING_WORLD -> {
+                this.worldService.createWorld();
+                this.loadingStep = FINISHED;
+            }
+            case FINISHED -> {
+                this.gameContext.getEcsWorld().progress(delta);
+                this.gameContext.getCursorManager().update(delta);
+                this.gameContext.getSettings().applyGraphicsSettings();
+                this.gameContext.getAssetManager().finishLoading();
+                this.screenManager.showNewGameScreen(this.worldService);
+            }
+        }
     }
 
     @Override
@@ -96,8 +101,8 @@ public class LoadScreen implements Screen {
     @Override
     public void dispose() {
         this.stage.dispose();
-        this.skin.dispose();
-        this.gameContext.getAssetManager().unload("loadingscreens/loadingscreens_skin.json");
-
+        if (this.gameContext.getAssetManager().isLoaded("loadingscreens/loadingscreens_skin.json")) {
+            this.gameContext.getAssetManager().unload("loadingscreens/loadingscreens_skin.json");
+        }
     }
 }
