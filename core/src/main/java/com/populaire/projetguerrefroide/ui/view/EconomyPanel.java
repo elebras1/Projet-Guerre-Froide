27c99689b68903f1ee7c91fb1cd704481867c750
elebras1 <erwan.lebras27@gmail.com>
2026-01-25 11:19:10 +0100

    economy panel impprovement

--- core/src/main/java/com/populaire/projetguerrefroide/ui/view/EconomyPanel.java
+++ core/src/main/java/com/populaire/projetguerrefroide/ui/view/EconomyPanel.java
@@ -16,6 +16,7 @@ import com.populaire.projetguerrefroide.ui.widget.HoverScrollPane;
 import com.populaire.projetguerrefroide.ui.widget.WidgetFactory;
 import com.populaire.projetguerrefroide.util.ValueFormatter;
 
+import java.util.List;
 import java.util.Map;
 
 public class EconomyPanel extends Table {
@@ -93,113 +94,203 @@ public class EconomyPanel extends Table {
         this.addActor(rightTable);
     }
 
-    @SuppressWarnings("unchecked")
     public void setData(RegionsBuildingsDto dto) {
-        Label.LabelStyle labelStyleJockey16GlowBlue = this.labelStylePool.get("jockey_16_glow_blue");
-        Label.LabelStyle labelJockey14Tight = this.labelStylePool.get("jockey_14_tight");
-        Drawable regionBackground = this.skin.getDrawable("economy_region_plate_small");
-
-        SnapshotArray<Actor> actors = this.buildingRegionsTable.getChildren();
+        SnapshotArray<Actor> currentActors = this.buildingRegionsTable.getChildren();
         int index = 0;
 
         for (RegionDto region : dto.regions()) {
-            String regionName = this.localisation.get(region.regionId());
-
-            if (index >= actors.size) {
-                Table regionTable = new Table();
-                regionTable.setBackground(regionBackground);
-                regionTable.setSize(regionBackground.getMinWidth(), regionBackground.getMinHeight());
-
-                this.widgetFactory.createLabel(regionName, labelStyleJockey16GlowBlue, 8, 7, regionTable);
-                Button minButton = this.widgetFactory.createButton(this.skin, "eco_plate_min", 684, 9, regionTable);
-                Button maxButton = this.widgetFactory.createButton(this.skin, "eco_plate_max", 684, 9, regionTable);
-                minButton.setVisible(true);
-                maxButton.setVisible(false);
-
-                this.widgetFactory.createLabelCentered(region.developpementIndexValue() + "%", labelStyleJockey16GlowBlue, 477, 7, regionTable);
-                this.widgetFactory.createLabelCentered(ValueFormatter.format(region.populationAmount(), this.localisation), labelStyleJockey16GlowBlue, 545, 7, regionTable);
-                this.widgetFactory.createLabelCentered(region.buildingWorkerAmount() + " (" + region.buildingWorkerRatio() + "%)", labelStyleJockey16GlowBlue, 638, 7, regionTable);
-
-                Table buildingsTable = new Table();
-                int economyCount = 0;
-
-                for (BuildingDto building : region.buildings()) {
-                    Table buildingTable = new ClickableTable();
-                    buildingTable.setUserObject(building.buildingId());
-                    String buildingLevel = building.buildingValue() + "/" + building.maxLevel();
-
-                    this.widgetFactory.applyBackgroundToTable(this.skin, "building_box_template", buildingTable);
-                    this.widgetFactory.createImage(this.skin, "building_" + building.buildingName(), 10, 55, buildingTable);
-                    this.widgetFactory.createLabelCentered(ValueFormatter.format(building.productionValue()), labelStyleJockey16GlowBlue, buildingTable.getWidth() / 2, 21, buildingTable);
-                    this.widgetFactory.createLabelCentered(buildingLevel, labelJockey14Tight, buildingTable.getWidth() / 2, 2, buildingTable);
-                    buildingsTable.add(buildingTable).padRight(-10);
-                    economyCount++;
-                    if (economyCount % 6 == 0) {
-                        buildingsTable.row();
-                    }
-                }
-
-                if (economyCount == 0) {
-                    for (int i = 0; i < 6; i++) {
-                        Table emptyTable = new Table();
-                        this.widgetFactory.applyBackgroundToTable(this.skin, "eco_build", emptyTable);
-                        buildingsTable.add(emptyTable).padRight(-10);
-                    }
-                } else {
-                    int remainder = economyCount % 6;
-                    if (remainder != 0) {
-                        int missing = 6 - remainder;
-                        for (int i = 0; i < missing; i++) {
-                            Table emptyTable = new Table();
-                            this.widgetFactory.applyBackgroundToTable(this.skin, "eco_build", emptyTable);
-                            buildingsTable.add(emptyTable).padRight(-10);
-                        }
-                    }
-                }
-
-                Table regionsBuildingsTable = new Table();
-                regionsBuildingsTable.add(regionTable).row();
-                Cell<Table> buildingCell = regionsBuildingsTable.add(buildingsTable).padLeft(-6);
-                regionsBuildingsTable.setUserObject(buildingsTable);
-
-                minButton.addListener(new ClickListener() {
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        buildingCell.clearActor();
-                        minButton.setVisible(false);
-                        maxButton.setVisible(true);
-                    }
-                });
-
-                maxButton.addListener(new ClickListener() {
-                    @Override
-                    public void clicked(InputEvent event, float x, float y) {
-                        buildingCell.setActor(buildingsTable);
-                        minButton.setVisible(true);
-                        maxButton.setVisible(false);
-                    }
-                });
-
-                this.buildingRegionsTable.add(regionsBuildingsTable).row();
+            if (index >= currentActors.size) {
+                Table newRegionBlock = this.createRegionBlock(region);
+                this.buildingRegionsTable.add(newRegionBlock).row();
             } else {
-                Actor actor = actors.get(index);
-                if (actor instanceof Table regionsBuildingsTable && regionsBuildingsTable.getChild(0) instanceof Table regionTable && regionTable.getChild(0) instanceof Label regionLabel && regionTable.getChild(1) instanceof Button minButton && regionTable.getChild(2) instanceof Button maxButton) {
-                    regionLabel.setText(regionName);
-                    if (maxButton.isVisible() && regionsBuildingsTable.getUserObject() instanceof Table buildingsTable) {
-                        Cell<Table> buildingCell = regionsBuildingsTable.getCells().get(1);
-                        buildingCell.setActor(buildingsTable);
-                        minButton.setVisible(true);
-                        maxButton.setVisible(false);
-                    }
-                }
+                this.updateRegionBlock((Table) currentActors.get(index), region);
             }
             index++;
         }
 
-        while (index < actors.size) {
-            Actor actor = actors.get(index);
-            actor.remove();
-            index++;
+        this.cleanupExcessActors(currentActors, index);
+    }
+
+    private Table createRegionBlock(RegionDto region) {
+        Table regionBlock = new Table();
+
+        Table headerTable = this.createRegionHeader(region);
+        Button minButton = headerTable.findActor("btn_min");
+        Button maxButton = headerTable.findActor("btn_max");
+
+        Table buildingsGrid = this.createBuildingsGrid(region.buildings());
+
+        regionBlock.add(headerTable).row();
+        Cell<Table> gridCell = regionBlock.add(buildingsGrid).padLeft(-6);
+
+        regionBlock.setUserObject(buildingsGrid);
+
+        this.setupCollapseListeners(minButton, maxButton, gridCell, buildingsGrid);
+
+        return regionBlock;
+    }
+
+    private Table createRegionHeader(RegionDto region) {
+        Table headerTable = new Table();
+        Drawable regionBackground = this.skin.getDrawable("economy_region_plate_small");
+        headerTable.setBackground(regionBackground);
+        headerTable.setSize(regionBackground.getMinWidth(), regionBackground.getMinHeight());
+
+        Label.LabelStyle style = this.labelStylePool.get("jockey_16_glow_blue");
+        String regionName = this.localisation.get(region.regionId());
+
+        Label nameLabel = this.widgetFactory.createLabel(regionName, style, 8, 7, headerTable);
+        nameLabel.setName("lbl_name");
+
+        Button minButton = this.widgetFactory.createButton(this.skin, "eco_plate_min", 684, 9, headerTable);
+        Button maxButton = this.widgetFactory.createButton(this.skin, "eco_plate_max", 684, 9, headerTable);
+        minButton.setName("btn_min");
+        maxButton.setName("btn_max");
+        minButton.setVisible(true);
+        maxButton.setVisible(false);
+
+        Label devIndexLabel = this.widgetFactory.createLabelCentered(region.developpementIndexValue() + "%", style, 477, 7, headerTable);
+        devIndexLabel.setName("lbl_dev_index");
+
+        Label popLabel = this.widgetFactory.createLabelCentered(ValueFormatter.format(region.populationAmount(), this.localisation), style, 545, 7, headerTable);
+        popLabel.setName("lbl_pop");
+
+        Label workLabel = this.widgetFactory.createLabelCentered(region.buildingWorkerAmount() + " (" + region.buildingWorkerRatio() + "%)", style, 638, 7, headerTable);
+        workLabel.setName("lbl_work");
+
+        return headerTable;
+    }
+
+    private Table createBuildingsGrid(List<BuildingDto> buildings) {
+        Table buildingsTable = new Table();
+        this.rebuildBuildingsGrid(buildingsTable, buildings);
+        return buildingsTable;
+    }
+
+    private void rebuildBuildingsGrid(Table buildingsTable, List<BuildingDto> buildings) {
+        buildingsTable.clearChildren();
+
+        int count = 0;
+        for (BuildingDto building : buildings) {
+            Table buildingCell = this.createBuildingItem(building);
+            buildingsTable.add(buildingCell).padRight(-10);
+
+            count++;
+            if (count % 6 == 0) {
+                buildingsTable.row();
+            }
+        }
+
+        this.fillEmptySlots(buildingsTable, count);
+    }
+
+    private Table createBuildingItem(BuildingDto building) {
+        Table buildingTable = new ClickableTable();
+        buildingTable.setUserObject(building.buildingId());
+
+        Label.LabelStyle labelStyle = this.labelStylePool.get("jockey_16_glow_blue");
+        Label.LabelStyle labelSmall = this.labelStylePool.get("jockey_14_tight");
+
+        this.widgetFactory.applyBackgroundToTable(this.skin, "building_box_template", buildingTable);
+        this.widgetFactory.createImage(this.skin, "building_" + building.buildingName(), 10, 55, buildingTable);
+
+        this.widgetFactory.createLabelCentered(ValueFormatter.format(building.productionValue()), labelStyle, buildingTable.getWidth() / 2, 21, buildingTable);
+
+        String levelText = building.buildingValue() + "/" + building.maxLevel();
+        this.widgetFactory.createLabelCentered(levelText, labelSmall, buildingTable.getWidth() / 2, 2, buildingTable);
+
+        return buildingTable;
+    }
+
+    private void fillEmptySlots(Table buildingsTable, int currentCount) {
+        int slotsNeeded = 0;
+
+        if (currentCount == 0) {
+            slotsNeeded = 6;
+        } else {
+            int remainder = currentCount % 6;
+            if (remainder != 0) {
+                slotsNeeded = 6 - remainder;
+            }
+        }
+
+        for (int i = 0; i < slotsNeeded; i++) {
+            Table emptyTable = new Table();
+            this.widgetFactory.applyBackgroundToTable(this.skin, "eco_build", emptyTable);
+            buildingsTable.add(emptyTable).padRight(-10);
+        }
+    }
+
+    private void setupCollapseListeners(Button minButton, Button maxButton, Cell<Table> gridCell, Table gridTable) {
+        minButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gridCell.clearActor();
+                minButton.setVisible(false);
+                maxButton.setVisible(true);
+            }
+        });
+
+        maxButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gridCell.setActor(gridTable);
+                minButton.setVisible(true);
+                maxButton.setVisible(false);
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private void updateRegionBlock(Table regionBlock, RegionDto region) {
+        if (regionBlock.getChildren().size < 1) {
+            return;
+        }
+
+        Table header = (Table) regionBlock.getChild(0);
+
+        Label nameLabel = header.findActor("lbl_name");
+        Label devIndexLabel = header.findActor("lbl_dev_index");
+        Label popLabel = header.findActor("lbl_pop");
+        Label workLabel = header.findActor("lbl_work");
+
+        if (nameLabel != null) {
+            nameLabel.setText(this.localisation.get(region.regionId()));
+        }
+
+        if (devIndexLabel != null) {
+            devIndexLabel.setText(region.developpementIndexValue() + "%");
+        }
+        if (popLabel != null) {
+            popLabel.setText(ValueFormatter.format(region.populationAmount(), this.localisation));
+        }
+        if (workLabel != null) {
+            workLabel.setText(region.buildingWorkerAmount() + " (" + region.buildingWorkerRatio() + "%)");
+        }
+
+        Button minButton = header.findActor("btn_min");
+        Button maxButton = header.findActor("btn_max");
+
+        Table buildingsTable = (Table) regionBlock.getUserObject();
+
+        if (buildingsTable != null) {
+            this.rebuildBuildingsGrid(buildingsTable, region.buildings());
+        }
+
+        if (maxButton != null && maxButton.isVisible() && buildingsTable != null) {
+            Cell<Table> cell = regionBlock.getCells().get(1);
+            cell.setActor(buildingsTable);
+            if(minButton != null) minButton.setVisible(true);
+            maxButton.setVisible(false);
+        }
+    }
+
+    private void cleanupExcessActors(SnapshotArray<Actor> actors, int startIndex) {
+        while (startIndex < actors.size) {
+            actors.get(startIndex).remove();
+        }
+        for (int i = actors.size - 1; i >= startIndex; i--) {
+            actors.get(i).remove();
         }
     }
 }
