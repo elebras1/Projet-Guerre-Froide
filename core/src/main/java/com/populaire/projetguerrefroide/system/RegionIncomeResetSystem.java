ebf8d7da3410690f08206a1a491e3b281f772522
elebras1 <erwan.lebras27@gmail.com>
2026-05-03 19:31:51 +0200

    fix RegionIncomeResetSystem

--- core/src/main/java/com/populaire/projetguerrefroide/system/RegionIncomeResetSystem.java
+++ core/src/main/java/com/populaire/projetguerrefroide/system/RegionIncomeResetSystem.java
@@ -2,18 +2,18 @@ package com.populaire.projetguerrefroide.system;
 
 import com.github.elebras1.flecs.Iter;
 import com.github.elebras1.flecs.World;
-import com.populaire.projetguerrefroide.component.RegionInstance;
+import com.populaire.projetguerrefroide.component.RegionInstanceIncome;
 
 public class RegionIncomeResetSystem {
 
     public RegionIncomeResetSystem(World ecsWorld, long phaseId) {
         ecsWorld.system("RegionIncomeResetSystem")
             .kind(phaseId)
-            .with(RegionInstance.class)
+            .with(RegionInstanceIncome.class)
             .iter(this::reset);
     }
 
     private void reset(Iter iter) {
-        iter.table().resetColumn(RegionInstance.class);
+        iter.table().resetColumn(RegionInstanceIncome.class);
     }
 }
