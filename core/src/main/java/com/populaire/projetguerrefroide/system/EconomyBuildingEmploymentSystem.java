d3b6f0441f8b3f33e6f78a956c7e0299c23a15bc
elebras1 <erwan.lebras27@gmail.com>
2026-05-08 09:58:01 +0200

    add next systeme todo

---
+++ core/src/main/java/com/populaire/projetguerrefroide/system/EconomyBuildingEmploymentSystem.java
@@ -0,0 +1,10 @@
+package com.populaire.projetguerrefroide.system;
+
+import com.github.elebras1.flecs.World;
+
+public class EconomyBuildingEmploymentSystem {
+
+    public EconomyBuildingEmploymentSystem(World ecsWorld, long phaseId) {
+        // TODO
+    }
+}
